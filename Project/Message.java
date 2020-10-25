c789ce28f71b53152324d7e6904fe850a4af3cfd
Kyle <k.cheng@ufl.edu>
2020-10-25 18:43:40 -0400

    changed Message, added un/choke, un/interested

--- Project/Message.java
+++ Project/Message.java
@@ -1,23 +1,39 @@
 public class Message {
-    private final int mLength;
-    private final byte mType;
-    private final String mPayload;
+    private int mLength;
+    private byte mType;
+    private byte[] mPayload;
 
-    public Message(byte type, String payload) {
-        mLength = 1 + payload.getBytes().length;
+    public Message(byte type, byte[] payload) {
+        mLength = 1 + payload.length;
         mType = type;
         mPayload = payload;
+    }
+
+    public Message(byte type){
+        mType = type;
     }
 
     public int getMLength() {
         return mLength;
     }
 
+    public void setMLength(int len){
+        this.mLength = len;
+    }
+
     public byte getMType() {
         return mType;
     }
 
-    public String getMPayload() {
+    public void setMType(byte type){
+        this.mType = type;
+    }
+
+    public byte[] getMPayload() {
         return mPayload;
+    }
+
+    public void setMPayload(byte[] pl){
+        this.mPayload = pl;
     }
 }
